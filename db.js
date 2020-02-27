e3a39730d3937cf2e156f364b68c79eb5368b1e5
Mellieejj <Melanieverheul1992@gmail.com>
2020-02-27 11:58:29 +0100

    force table

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const databaseUrl =
 
 const db = new Sequelize(databaseUrl);
 
-db.sync()
+db.sync({ force: true })
   .then(() => console.log("database updated"))
   .catch(console.error);
 
--- player/model.js
+++ player/model.js
@@ -1,9 +1,11 @@
 const Sequelize = require("sequelize");
 const db = require("../db.js");
+const Team = require("../team/model");
 
 const Player = db.define("player", {
   name: Sequelize.STRING,
   number: Sequelize.INTEGER
 });
 
+Player.belongsTo(Team);
 module.exports = Player;
